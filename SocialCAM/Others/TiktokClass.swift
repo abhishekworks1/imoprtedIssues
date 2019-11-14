3a9561619429521ac6420e6db30fc3cce342ab1e
Viraj Patel <viraj.p@simformsolutions.com>
2019-11-14 16:09:01 +0530

    XT1-T5299 Change App Name and Project Name (+1 squashed commit) Squashed commits: [5660f9f] New App Icon Replace with old. App Name Change with 'SocialCAM'

---
+++ SocialCAM/Others/TiktokClass.swift
@@ -0,0 +1,36 @@
+//
+//  TiktokClass.swift
+//  SocialCAM
+//
+//  Created by Viraj Patel on 13/11/19.
+//  Copyright © 2019 Viraj Patel. All rights reserved.
+//
+
+import Foundation
+import TikTokOpenPlatformSDK
+
+open class TiktokShare: NSObject, TikTokOpenPlatformLogDelegate {
+    
+    static let shared: TiktokShare = TiktokShare()
+    
+    var delegate: ShareStoriesDelegate?
+    
+    var isTiktokInstalled: Bool {
+        return TiktokOpenPlatformApplicationDelegate.sharedInstance().isAppInstalled(with: TikTokOpenPlatformAppType.I18N)
+    }
+    
+    public override init() {
+        super.init()
+        
+    }
+
+    func setupTiktok(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) {
+    
+        TiktokOpenPlatformApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        TiktokOpenPlatformApplicationDelegate.sharedInstance().logDelegate = self
+    }
+    
+    public func tiktok(onLog logInfo: String) {
+        debugPrint(logInfo)
+    }
+}
