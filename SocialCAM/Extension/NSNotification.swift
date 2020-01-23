cde454b26c77be6605dfb616826cc5dbc5bd66de
Jasmin Patel <jasmin.p@simformsolutions.com>
2020-01-23 17:39:05 +0530

    TA1-T5412: ClientFeedback 13 Jan 2020 - Fixed volume button issue

---
+++ SocialCAM/Extension/NSNotification.swift
@@ -0,0 +1,15 @@
+//
+//  NSNotification.swift
+//  SocialCAM
+//
+//  Created by Jasmin Patel on 23/01/20.
+//  Copyright © 2020 Viraj Patel. All rights reserved.
+//
+
+import Foundation
+
+extension NSNotification.Name {
+    
+    public static let AVSystemVolumeDidChange = NSNotification.Name(rawValue: "AVSystemController_SystemVolumeDidChangeNotification")
+    
+}
