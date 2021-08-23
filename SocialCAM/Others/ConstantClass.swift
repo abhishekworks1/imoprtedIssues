7aa54abf29c9376cffa803d3b758736b20b9bfcb
Nilisha gupta <nilisha.g@simformsolutions.com>
2021-08-23 16:04:34 +0530

    XT1-T6270: Update application survey link

--- SocialCAM/Others/ConstantClass.swift
+++ SocialCAM/Others/ConstantClass.swift
@@ -60,7 +60,7 @@ public struct Constant {
         static let pic2ArtWebsiteURL = "https://pic2art.iicc.online"
         static let soccercamWebsiteURL = "http://soccercam.iicc.online"
         static let futbolWebsiteURL = "http://futbolcam.iicc.online"
-        static let applicationSurveyURL = "https://docs.google.com/forms/d/e/1FAIpQLSfjHUxhARTecUA39brgD0XunJdnOgJ9QUhfnH-k-yrAk-nUnA/viewform"
+        static let applicationSurveyURL = "https://docs.google.com/forms/d/e/1FAIpQLSevMPz6yVq56rmVcprOfZ9kSckBbrq9XNZXUFAb7zAuBQZDbw/viewform"
     }
     
     struct Application {
