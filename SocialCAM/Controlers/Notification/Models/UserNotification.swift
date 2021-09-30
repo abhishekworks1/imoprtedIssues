368dbbac2ec4d408d6d2c77d26d9894a31647c0a
Viraj Patel <viraj.p@simformsolutions.com>
2021-09-30 22:36:28 +0530

    XT1-T6476: The selected flags from a previous flag selection should display at the top when the user returns to the flag selection option.

---
+++ SocialCAM/Controlers/Notification/Models/UserNotification.swift
@@ -0,0 +1,44 @@
+//
+//  UserNotification.swift
+//  SocialCAM
+//
+//  Created by Viraj Patel on 01/10/21.
+//  Copyright © 2021 Viraj Patel. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class UserNotification: Mappable {
+
+    var id: String?
+    var createdAt: String?
+    var isRead: Bool?
+    var isSendEmail: Bool?
+    var isSendPush: Bool?
+    var isSendSms: Bool?
+    var message: String?
+    var notificationType: Int?
+    var platformType: String?
+    var refereeUserId: RefereeUserId?
+    var title: String?
+    var updatedAt: String?
+
+    required init?(map: Map){}
+
+    func mapping(map: Map) {
+        id <- map["_id"]
+        createdAt <- map["createdAt"]
+        isRead <- map["isRead"]
+        isSendEmail <- map["isSendEmail"]
+        isSendPush <- map["isSendPush"]
+        isSendSms <- map["isSendSms"]
+        message <- map["message"]
+        notificationType <- map["notification_type"]
+        platformType <- map["platform_type"]
+        refereeUserId <- map["refereeUserId"]
+        title <- map["title"]
+        updatedAt <- map["updatedAt"]
+        
+    }
+}
