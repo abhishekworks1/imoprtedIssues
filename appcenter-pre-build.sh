2bf5acb9977cd702d70e210628956cd01522cea3
shrinivas murthy <shrimurthy178@gmail.com>
2022-03-15 16:38:28 +0000

    Add new file

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Hello Prebuild Script"
+VERSION_CODE=$((VERSION_CODE_SHIFT + APPCENTER_BUILD_ID))
+plutil -replace CFBundleVersion -string "$VERSION_CODE" $APPCENTER_SOURCE_DIRECTORY/QuickCam-Info.plist
+echo "Hello Prebuild Script END"
+cat $APPCENTER_SOURCE_DIRECTORY/QuickCam-Info.plist
