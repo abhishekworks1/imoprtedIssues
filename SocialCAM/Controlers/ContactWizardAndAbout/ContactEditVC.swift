39dbf30d6c8c378833ab97b2bd1dc9de9ba2074a
Ideveloper <siddharthiosdev@gmail.com>
2022-05-23 19:31:10 +0530

    shadow added

--- SocialCAM/Controlers/ContactWizardAndAbout/ContactEditVC.swift
+++ SocialCAM/Controlers/ContactWizardAndAbout/ContactEditVC.swift
@@ -108,7 +108,7 @@ extension UIView {
         layer.masksToBounds = false
         layer.shadowColor = UIColor.lightGray.cgColor
         layer.shadowOpacity = 0.7
-        layer.shadowOffset = CGSize(width: -1, height: 1)
+        layer.shadowOffset = CGSize(width: 1, height: 2)
         layer.shadowRadius = 1
         layer.shouldRasterize = true
         layer.rasterizationScale = scale ? UIScreen.main.scale : 1
