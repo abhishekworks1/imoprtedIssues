a9ca87940a9fc64b4aa904fb5f6318370401a1b4
Charmi <>
2022-06-07 13:47:41 +0530

    Update ContactImportVC.swift

--- SocialCAM/Controlers/ContactWizardAndAbout/ContactImportVC.swift
+++ SocialCAM/Controlers/ContactWizardAndAbout/ContactImportVC.swift
@@ -1980,7 +1980,7 @@ class ContactImportVC: UIViewController, UITableViewDelegate, UITableViewDataSou
         
 //        if let token = Defaults.shared.sessionToken {
 //        let urlString = "\(websiteUrl)/redirect?token=\(token)"
-        let urlString = "\(websiteUrl)/share-wizard"
+        let urlString = "\(websiteUrl)/share-wizard?redirect_uri=\(redirectUri)"
         guard let url = URL(string: urlString) else {
                 return
             }
