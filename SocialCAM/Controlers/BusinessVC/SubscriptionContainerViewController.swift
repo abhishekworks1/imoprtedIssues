7917ce3e92ee0debbce768d4af99c6e8242258ef
Charmi <>
2022-06-28 11:37:31 +0530

    New Badges Updated

--- SocialCAM/Controlers/BusinessVC/SubscriptionContainerViewController.swift
+++ SocialCAM/Controlers/BusinessVC/SubscriptionContainerViewController.swift
@@ -151,6 +151,7 @@ class SubscriptionContainerViewController: UIViewController {
                         lblBadgeBasic.text = ""
                         lblBadgeAdvanced.text = ""
                         lblBadgePro.text = ""
+//                        showFreeTimer(createdDate: createdDate)
                     }
                 }
             }
@@ -172,7 +173,21 @@ class SubscriptionContainerViewController: UIViewController {
             }
         }
     }
-    
+    func showFreeTimer(createdDate: Date){
+        timerLabel.isHidden = false
+        var dateComponent = DateComponents()
+        dateComponent.day = 7
+        if let futureDate = Calendar.current.date(byAdding: dateComponent, to: createdDate) {
+            self.countdownTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+                let countdown = Calendar.current.dateComponents([.day, .hour, .minute, .second], from: Date(), to: futureDate)
+                let days = countdown.day!
+                let hours = countdown.hour!
+                let minutes = countdown.minute!
+                let seconds = countdown.second!
+                self.timerLabel.text = String(format: "%01dd : %02dh : %02dm : %02ds", days, hours, minutes, seconds)
+            }
+        }
+    }
     @IBAction func didTapPremiumButton(_ sender: UIButton) {
         guard let subscriptionVc = R.storyboard.subscription.subscriptionsViewController() else { return }
         subscriptionVc.appMode = .professional
